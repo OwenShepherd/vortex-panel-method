c2be37029b98c31c1ece6d8da69d37d4a952ce0a
user <owsh8290@colorado.edu>
2017-09-28 21:18:45 -0600

    Created basic project structure and added initial setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='vpm',
+      version='0.01',
+      description='Vortex Panel Method',
+      long_description='Adapted for Python from Kuethe and Chow'
+      url='https://github.com/OwenShepherd/vortex-panel-method',
+      author='KCO',
+      author_email='owen.shepherd@colorado.edu',
+      license='MIT',
+      packages=['vpm'],
+      zip_safe=False
+      include_package_data=True)
